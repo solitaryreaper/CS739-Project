eade4da2ea74874da8e44cf009d3b22019dc44fb
Prakhar Panwaria <prakhar.panwaria9@gmail.com>
2013-12-08 21:41:15 -0600

    Added server replication.

--- app/controllers/Servermeta.java
+++ app/controllers/Servermeta.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import models.Constants;
+import models.dao.DBService;
+import models.dao.RelationalDBService;
 
 import org.codehaus.jackson.node.ObjectNode;
 
@@ -25,6 +27,30 @@ public class Servermeta extends Controller {
 	 */
 	public static Result getHeartBeat()
 	{
+		ObjectNode result = Json.newObject();
+		result.put(Constants.LATEST_HEARTBEAT, System.currentTimeMillis());
+		return ok(result);
+	}
+	
+	/**
+	 * Method gets called from another server to replicate its data to this server.
+	 * 
+	 * @param paintRoom		Paint Room Name
+	 * @param painter		Painter Name
+	 * @param startX		Start Point X
+	 * @param startY		Start Point Y
+	 * @param endX			End Point X
+	 * @param endY			End Point Y
+	 * @return
+	 */
+	public static Result replicateData(String paintRoom, String painter, int startX, int startY, int endX, int endY)
+	{
+		// Starting a DB Service to write the points coming in from another server to the local database.
+		DBService dbService = new RelationalDBService();
+		dbService.insertPaintBrushEvents(paintRoom, painter, startX, startY, endX, endY);
+		
+		// TODO: Not sure if returning result is necessary. If yes,
+		// Returning the latest timestamp as heartbeat.
 		ObjectNode result = Json.newObject();
 		result.put(Constants.LATEST_HEARTBEAT, System.currentTimeMillis());
 		return ok(result);
--- app/models/utils/AppUtils.java
+++ app/models/utils/AppUtils.java
@@ -2,6 +2,12 @@ package models.utils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+import play.Logger;
+import play.api.libs.concurrent.Promise;
+import play.api.libs.ws.Response;
+import play.api.libs.ws.WS;
 
 /**
  * Simple utility functions for this application.
@@ -10,7 +16,55 @@ import java.net.UnknownHostException;
  *
  */
 public class AppUtils {
+	
+	public static final String HTTP_STR = "http://";
+	public static final String WORKER_SERVER_URL = ":9000/canvas?";
+	public static final String PAINTROOM = "paintroom";
+	public static final String PAINTER = "painter";
+	public static final String BEGINX = "bX";
+	public static final String BEGINY = "bY";
+	public static final String ENDX = "eX";
+	public static final String ENDY = "eY";
+	
+	/**
+	 * Call replicate API with these parameters.
+	 */
+	public static boolean replicateDataOnServers(ArrayList<String> destServers, String paintRoom, String painterName, 
+											  int beginX, int beginY, int endX, int endY)
+	{
+		// If no servers to update.
+		if(destServers == null || destServers.isEmpty()) return false;
+		
+		// Send this data to all the servers in the list
+		for(String ipAddress : destServers)
+		{
+			Logger.info("Sending the data to [" + ipAddress + "] worker server.");
+			try 
+			{
+				String url = HTTP_STR + ipAddress + WORKER_SERVER_URL + 
+								PAINTROOM 	+ "=" + paintRoom 	+ "&" +
+								PAINTER 	+ "=" + painterName + "&" +
+								BEGINX 		+ "=" + beginX 		+ "&" +
+								BEGINY 		+ "=" + beginY 		+ "&" +
+								ENDX 		+ "=" + endX 		+ "&" +
+								ENDY 		+ "=" + endY;
+				
+				Logger.info("Server_Replication :: URL Generated" + url);
+				Promise<Response> body = WS.url(url).get();
+				String result = body.value().get().body();
+				Logger.info("Result from Server: " + result);
 
+				// if(result == null), Do nothing!
+			} 
+			catch (Exception e) 
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
 	/**
 	 * Gets the local IP address.
 	 * @return
